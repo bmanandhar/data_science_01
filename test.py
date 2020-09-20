005a8b60d8bc85c34a2cab58b2f191ca3a3a1667
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2020-09-19 19:53:43 -0700

    commit first

---
+++ test.py
@@ -0,0 +1,46 @@
+import csv
+
+male = female = other= 0
+with open('test.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+
+    for line in csv_reader:
+        if line[1] == 'Male':
+            male += 1
+        elif line[1] == 'Female':
+            female += 1
+        else:
+            print(line[1])
+            other += 1
+print(line)
+print('Male: ', male, 
+    ' Female: ', female, 
+    ' Other: ', other)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# f = open("test.txt", 'r')
+# print(f.readline())
+
+# f.close()
+# f = open("test.txt", "w")
+# f.write("I did not want to delete the content, but it got deleted.")
+# f.close()
+# open("onefile.txt", "a")
+# # open("myfile.txt", "x")
+# f = open("yourfile.txt", "w")
