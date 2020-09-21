b50f3e997f634445e652151dc4a79db618bfc522
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2020-09-21 08:49:33 -0700

    additional statistical values

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 #['508146', 'Male', '41', '1', '29.0', '1', '1-2 Year', 'No', '27927.0', '124.0', '231']
 import csv
 import numpy as np
+from scipy import stats
 
 age_list = []
 male = female = 0
@@ -16,11 +17,22 @@ with open('test.csv', 'r') as csv_file:
         else: other += 1
 
         age_list.append(int(line[2]))
-
+#prints total num of members
 print('Total members: ', len(age_list))
+#prints percentile 75
 print('Percentile: ', np.percentile(age_list, 75))
+#prints avg age of members
+print('Average age: ', int(round(np.mean(age_list))))
+#prints median age
+print('Median age: ', np.median(age_list))
+#prints mode of ages
+print('Mode of ages: ', stats.mode(age_list))
+#prints total male, female
 print('Male: {}\nFemale: {}'.format(male, female))
 
+# path = "F:\data\google_stock_data.csv"
+# file = open(path)
+# for line in file: print(line)
 
 
 
