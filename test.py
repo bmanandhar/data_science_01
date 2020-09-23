9030f6375ce2403e9344fca32eb5c05ad3a1c955
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2020-09-22 22:00:03 -0700

    function_file added

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 #['508146', 'Male', '41', '1', '29.0', '1', '1-2 Year', 'No', '27927.0', '124.0', '231']
 import csv
 import numpy as np
-import function_file
+import function_file as f 
 
 age_list = []
 male = female = 0
@@ -15,16 +15,17 @@ with open('test.csv', 'r') as csv_file:
         if line[1] == 'Male': male += 1
         elif line[1] == 'Female': female += 1
         age_list.append(int(line[2]))
+
 #prints total num of members
 print('Total members: ', len(age_list))
 #prints percentile 75
-print('Percentile: ', function_file.func_percentile(age_list, 75))
+print('Percentile: ', f.func_percentile(age_list, 75))
 #prints avg age of members
-print('Average age: ', int(round(function_file.func_mean(age_list))))
+print('Average age: ', int(round(f.func_mean(age_list))))
 #prints median age
-print('Median age: ', function_file.func_median(age_list))
+print('Median age: ', f.func_median(age_list))
 #prints mode of ages
-print('Mode of ages: ', function_file.func_mode(age_list))
+print('Mode of ages: ', f.func_mode(age_list))
 #prints total male, female
 print('Male: {}\nFemale: {}'.format(male, female))
 
